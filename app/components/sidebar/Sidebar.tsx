8b2f82dcf662ba06d1434ca3ba0d66f6bcc68e4a
Sebastian Rojas <sebastianrojas812@gmail.com>
2023-05-11 16:45:01 -0400

    finish desktop side bar, mobile view and actions for session and getting current user

--- app/components/sidebar/Sidebar.tsx
+++ app/components/sidebar/Sidebar.tsx
@@ -1,10 +1,17 @@
+import getCurrentUser from "@/app/actions/getCurrentUser";
 import DesktopSidebar from "./DesktopSidebar";
+import MobileFooter from "./MobileFooter";
 
 async function Sidebar({ children }: { children: React.ReactNode }) {
+
+    const currentUser = await getCurrentUser();
+
+
     return (
         <div className="h-full">
             <main className="h-full lg:pl-20">
-                <DesktopSidebar />
+                <DesktopSidebar currentUser={currentUser!} />
+                <MobileFooter />
                 {children}
             </main>
         </div>
